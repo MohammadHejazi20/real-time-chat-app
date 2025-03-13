b6d92d3e4d29b2e3a1926d19ff3ca3e2367a525c
Mohammad Hejazi <89161570+MohammadHejazi20@users.noreply.github.com>
2025-03-13 17:11:50 +0100

    chore: Remove unused ESLint and Tailwind CSS configurations, update package dependencies, and add new SVG assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,155 +1,103 @@
-"use client";
+import Image from "next/image";
 
-import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
-
-type MessageType = {
-  userId: string;
-  userMessage: string;
-  userName: string;
-};
-const socket = io("http://localhost:3001");
-
-const Home = () => {
-  const [name, setName] = useState<string>("");
-  const [nameSet, setNameSet] = useState<boolean>(false);
-  const [messages, setMessages] = useState<MessageType[]>([]);
-  const [newMessage, setNewMessage] = useState("");
-
-  const [users, setUsers] = useState<{ id: string; name: string }[]>([]);
-
-  useEffect(() => {
-    socket.on("chat message", (data) => {
-      setMessages((prevMessages) => [...prevMessages, data]);
-    });
-
-    socket.on("users list", (users) => {
-      setUsers(users);
-    });
-
-    return () => {
-      socket.off("chat message");
-      socket.off("users list");
-    };
-  }, []);
-
-  const sendMessage = () => {
-    if (newMessage.trim() === "") {
-      return;
-    }
-    socket.emit("chat message", { id: socket.id, message: newMessage });
-    setNewMessage("");
-  };
-
-  const handleSetName = () => {
-    if (name) {
-      socket.emit("set name", name);
-      setNameSet(true);
-    }
-  };
-
+export default function Home() {
   return (
-    <div className="mx-auto">
-      <div className="navbar bg-primary text-primary-content rounded-sm">
-        <button className="btn btn-ghost text-xl text-cyan-50">
-          Real-Time Chat
-        </button>
-      </div>
-      {!nameSet ? (
-        <div className="hero bg-base-200 min-h-screen">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-              <h1 className="text-5xl font-bold">Join Chat</h1>
-            </div>
-            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-              <form className="card-body">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Name</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Enter your name"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="input input-bordered"
-                    required
-                  />
-                </div>
-                <div className="form-control mt-6">
-                  <button className="btn btn-primary" onClick={handleSetName}>
-                    Join
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="">
-          <div className="indicator m-5">
-            <span className="indicator-item indicator-start badge badge-secondary"></span>
-            <div className="bg-base-300 grid h-auto w-auto place-items-center rounded-md p-3 gap-3">
-              <h2 className="text-xl font-bold mb-2">Connected Users</h2>
-              <div className="">
-                {users.map((user) => (
-                  <div
-                    key={user.id}
-                    className="avatar online placeholder gap-2"
-                  >
-                    <div className="bg-neutral text-neutral-content w-16 rounded-full">
-                      <span className="text-xl">{user.name}</span>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-          <div className="flex flex-col m-5">
-            <h2 className="text-xl font-bold mb-2">Messages</h2>
-            <div className="overflow-y-auto w-full">
-              <div className="indicator w-full">
-                <div className="card border w-full">
-                  <div className="card-body w-full">
-                    {messages.map((message, index) => (
-                      <div key={index} className="chat chat-start ">
-                        <div className="chat-bubble chat-bubble-success">
-                          <span className="text-gray-500">
-                            {message.userName}:{" "}
-                          </span>
-                          {message.userMessage}
-                        </div>
-                      </div>
-                    ))}
-                    <div className="join w-full">
-                      <input
-                        className="input input-bordered join-item w-full rounded-l-full"
-                        type="text"
-                        value={newMessage}
-                        onChange={(e) => setNewMessage(e.target.value)}
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter") {
-                            sendMessage();
-                          }
-                        }}
-                      />
-                      <button
-                        className="btn join-item rounded-r-full"
-                        onClick={sendMessage}
-                      >
-                        Send
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-      )}
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
-};
-
-export default Home;
+}
